97c31b42a6eaa2b8b9f2dcfb1f3c9a0dd8cd94ad
hxj413977035 <413977035@qq.com>
2018-08-21 22:56:45 +0800

    二分搜索树

--- src/main/java/com/info/tree/binarySearchTree/BinarySearchTree.java
+++ src/main/java/com/info/tree/binarySearchTree/BinarySearchTree.java
@@ -243,6 +243,50 @@ public class BinarySearchTree<E extends Comparable<E>> {
         return node;
     }
 
+    // 从二分搜索树中删除元素为e的节点
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    // 删除以node为根的二分搜索树中值为e的节点  递归算法
+    // 返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else {
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            // 待删除的节点左右子树均不为空
+            // 找到比待删除节点大的最小节点，即待删除节点的右子树最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = getMin(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
+
     public static void main(String[] args) {
         BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<Integer>();
         int[] nums = {5, 3, 6, 8, 4, 2};
